995526f373637c4a0a929a5723a27c45e3971251
Joseph Gramling <jgramling17@jcu.edu>
2017-04-06 17:03:53 -0400

    Gmail API is now working

--- src/app/services/calendar-auth.service.ts
+++ src/app/services/calendar-auth.service.ts
@@ -1,18 +1,24 @@
 export class CalendarAuthService {
     static clientID = '386660134986-n5l4lur2aroupk20fedhph66vibc50q9.apps.googleusercontent.com'
     static scope = 'https://www.googleapis.com/auth/calendar.readonly'
+    static APIKey = 'AIzaSyCBN28Xf182sjJ6yYBgBm9XGDL6H1A1v24'
 
-    //public gapi: any;
     public userName: String;
     public isAuthenticated: Boolean = false;
 
     constructor() {
-        this.authenticate();
+        console.log('initializing calendar authentication service;');
+        this.runLifeCycle();
+    }
+
+    runLifeCycle(){
+        return this.authenticate()
+		 .then(() => this.getEvents())
     }
 
     authenticate() {
         return new Promise((resolve, reject) => {
-            console.log('made it this far');
+        gapi.client.setApiKey(CalendarAuthService.APIKey);
         var authorisationRequestData =
 			{
 				client_id: CalendarAuthService.clientID, 
@@ -22,7 +28,8 @@ export class CalendarAuthService {
        gapi.auth.authorize(authorisationRequestData, (token) => {
            if (token && !token.error) 
            {
-               console.log('authenticated yo');
+               console.log('authenticated');
+               console.log(token);
                 this.isAuthenticated = true;
                 resolve();
            }
@@ -34,6 +41,33 @@ export class CalendarAuthService {
            }
         })
         });
+    }
+
+    getEvents(){
+            var currentDate = new Date();
+            var maxDate = new Date()
+            maxDate.setHours(currentDate.getHours() + 12);
+            var currentDateISO = currentDate.toISOString();
+            var maxDateISO = maxDate.toISOString();
+            console.log(currentDateISO);
+            console.log(maxDateISO);
+            return new Promise((resolve, reject) => {
+                console.log('initialize Google Calendar API');
+                gapi.client.load('calendar', 'v3', function callApi(){
+                     var request = gapi.client.calendar.events.list({
+                        calendarId: 'primary',
+                        singleEvents: true,
+                        timeMax: maxDateISO,
+                        timeMin: currentDateISO
+	            });
+                request.execute((resp) => {
+	        	var appointments = [];
+                console.log(resp);
+            })
+                });
+               
+        })
+        
     }
     
 }
